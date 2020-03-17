b55728962faee72ed2e74fdf9d367f00fa50ff08
pukaifei <kaifei.pu@aispeech.com>
2020-03-17 12:37:34 +0800

    add vim74, vimgdb example

---
+++ vimgdb/example/factor/factor.c
@@ -0,0 +1,13 @@
+/* ~/tmp/factor/factor.c */
+int factor(int n, int *r)
+{
+    if (n <= 1)
+        *r =  n;
+    else
+    {
+        factor(n - 1, r);
+        *r *= n;
+    }
+
+    return 0;
+}
